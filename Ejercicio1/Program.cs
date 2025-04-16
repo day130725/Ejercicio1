4440c36762c5f823a2d7737da25dcf9fcf09e41e
Dayana Rojas <N00439638@upn.pe>
2025-04-16 16:27:05 -0500

    Ejercicio 1 culminado

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -10,7 +10,44 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            //ax^2 + bc +c 
+            //2 Soluciones
+            //disc> 0 -> 2 sol.
+            //disc= 0 -> 1 sol.
+            //disc< 0 -> 0 sol.
+            int a, b, c, disc;
+            double x1, x2;
 
+            Console.Write("Ingrese a: ");
+            a= int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese c: ");
+            c = int.Parse(Console.ReadLine());
+
+            disc = (b * b) - (4 * a *  c);
+
+            if (disc > 0) 
+            {
+                x1 = (-b + Math.Sqrt(disc)) / (2 * a);
+                x2 = (-b - Math.Sqrt(disc)) / (2 * a);
+
+                Console.WriteLine("X1: " + x1);
+                Console.WriteLine("X2: " + x2);
+            } else 
+            {
+                if (disc==0) 
+                {
+                    x1 = -b/ 2 * a;
+                    Console.WriteLine("X1: " +x1);
+                }
+                else 
+                {
+                    Console.WriteLine("No tiene soluciones");
+                }
+            }
         }
     }
 }
